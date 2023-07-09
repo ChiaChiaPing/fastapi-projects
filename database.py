0321ae080426efa027778bf193b9209f1bb7e209
ChiaChiaPing <olk957153@gmail.com>
2023-07-09 16:12:40 +0800

    change database

--- database.py
+++ database.py
@@ -2,7 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+# sqlite
+#SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+
+# PostgrSsql: need hide password bro
+SQLALCHEMY_DATABASE_URL = "postgresql://dpzseuxc:m84zSOdGO_DQ6khgs5C8B9_gUj9J93WX@john.db.elephantsql.com/dpzseuxc"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
